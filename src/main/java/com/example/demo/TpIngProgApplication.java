0e1e4951a4ff55866fafb77b22f6d1737cb561f3
blmillaaGH <blasfranciscomillaa@gmail.com>
2024-10-17 19:46:27 -0300

    Subida inicial del proyecto Habit Tracker

---
+++ src/main/java/com/example/demo/TpIngProgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIngProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIngProgApplication.class, args);
+	}
+
+}
