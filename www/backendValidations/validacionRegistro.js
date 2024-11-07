ab5ddbc8a5a430baf0e9871a6bf4d85aa07544aa
blmillaaGH <blasfranciscomillaa@gmail.com>
2024-11-06 22:30:03 -0300

    version

--- www/backendValidations/validacionRegistro.js
+++ www/backendValidations/validacionRegistro.js
@@ -1,25 +1,27 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+document.getElementById("registroForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita el envío del formulario para manejar la validación y el envío personalizado
+
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    
+    const errorMessage = document.getElementById("error-message");
+
     fetch('http://localhost:8080/api/auth/registro', {
         method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({email,password})  
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })  
     })
     .then(response => {
-        if (response.ok){
+        if (response.ok) {
             return response.text();
         } else {
-            throw new Error("DATOS INVALIDOS");
+            throw new Error("Datos inválidos");
         }
     })
     .then(message => {
         alert(message);
-        window.location.href="main.html";
+        window.location.href = "main.html"; // Redirige si el registro es exitoso
     })
     .catch(error => {
-        document.getElementById("error-message").textContent = error.message;
-    })   
+        errorMessage.textContent = error.message; // Muestra el mensaje de error en el HTML
+    });
 });
